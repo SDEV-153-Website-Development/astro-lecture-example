2aed9191e23b4ba5fe680959865285e4299b010c
Eric Sexton <esexton15@ivytech.edu>
2026-01-28 16:35:05 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	base: "/repo-name", // replace 'repo-name' with your GitHub repository name
+});
